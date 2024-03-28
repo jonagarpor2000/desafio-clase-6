dbd0517e71f80168951382196cffb555ea542c50
Jonathan Garcia <jonathan.porley15@gmail.com>
2024-03-27 23:12:44 -0300

    First structure upload

---
+++ src/app.js
@@ -0,0 +1,61 @@
+import express from 'express'
+import ProductManager  from './ProductManager.js'
+const app = express()
+const pmg = new ProductManager()
+let users = []
+
+app.use(express.json)
+app.use(express.urlencoded({extended:true}))
+
+app.get('/products', (req, res) => {
+    res.send(`Test`)
+    //res.send({status:'success',payload:pmg.getProducts()})
+    //res.status(404).send("Coso")
+})
+//R
+app.get('/api/users/', (req, res) => {
+    res.send(users)
+})
+
+app.get('/api/users/:uid', (req, res) => {
+    res.send('get user')
+})
+
+//C
+app.post('/api/users/', (req, res) => {
+    //puedo definirlo como objeto con const user = req.body
+    const {first_name,last_name,email,password} = req.body
+    const newUser = {
+        id: users.length + 1,
+        first_name,
+        last_name,
+        email,
+        password
+    }
+    users.push(newUser)
+    res.send('Hola soy servidoreitor')
+})
+//U
+app.put('/api/users/:uid', (req, res) => {
+    const {uid} = req.params.uid
+    const userToUpdate = req.body
+    const userIndex  = users.findIndex(user => user.id === parseInt(uid))
+    if(userIndex===-1) return res.status(404).send({status:'error',error:'user not found'})
+    users[userIndex] = userToUpdate
+    res.send({status:'success',payload: userToUpdate})
+})
+
+//D
+app.delete('/api/users/:uid', (req, res) => {
+    const {uid} = req.params.uid
+    const UserResult  = users.filter(user => user.id === parseInt(uid))
+    if(UserResult===-1) return res.status(404).send({status:'error',error:'user not found'})
+    res.send({status:'success',payload: UserResult})
+})
+
+
+
+app.listen(8080,(error)=>{
+    if(error) return console.log(error)
+    console.log('Servidor escuchando en el puerto 8080')
+})
